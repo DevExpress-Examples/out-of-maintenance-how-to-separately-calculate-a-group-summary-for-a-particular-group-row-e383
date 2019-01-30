95743a7f1d3e33075a2a56eea79c4451de6c2129
DevExpressExampleBot <fakeEmail>
2019-01-30 17:22:47 +0300

    Source auto update [skip ci]

--- VB/CarList.vb
+++ VB/CarList.vb
@@ -1,5 +1,4 @@
-﻿Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Linq
@@ -7,74 +6,126 @@ Imports System.Text
 Imports System.Threading.Tasks
 
 Namespace summary
-	Public NotInheritable Class CarList
-		Private Sub New()
-		End Sub
-		Public Shared Function GetList() As BindingList(Of Car)
-			Dim list = New BindingList(Of Car)()
-			list.Add(New Car() With {.ID = 1, .Trademark = "Mercedes-Benz", .Model = "SL500 Roadster", .HP = 302, .Liter = 4.966})
-			list.Add(New Car() With {.ID = 2, .Trademark = "Mercedes-Benz", .Model = "CLK55 AMG Cabriolet", .HP = 342, .Liter = 5.439})
-			list.Add(New Car() With {.ID = 3, .Trademark = "Mercedes-Benz", .Model = "C230 Kompressor Sport Coupe", .HP = 189, .Liter = 1.796})
-			list.Add(New Car() With {.ID = 4, .Trademark = "BMW", .Model = "530i", .HP = 225, .Liter = 3})
-			list.Add(New Car() With {.ID = 5, .Trademark = "Rolls-Royce", .Model = "Corniche", .HP = 325, .Liter = 6.75})
-			list.Add(New Car() With {.ID = 6, .Trademark = "Jaguar", .Model = "S-Type 3.0", .HP = 235, .Liter = 3})
-			list.Add(New Car() With {.ID = 7, .Trademark = "Cadillac", .Model = "Seville", .HP = 275, .Liter = 4.6})
-			list.Add(New Car() With {.ID = 8, .Trademark = "Cadillac", .Model = "DeVille", .HP = 275, .Liter = 4.6})
-			list.Add(New Car() With {.ID = 9, .Trademark = "Lexus", .Model = "LS430", .HP = 290, .Liter = 4.3})
-			list.Add(New Car() With {.ID = 10, .Trademark = "Lexus", .Model = "GS 430", .HP = 300, .Liter = 4.3})
-			list.Add(New Car() With {.ID = 11, .Trademark = "Ford", .Model = "Ranger FX-4", .HP = 135, .Liter = 2.3})
-			list.Add(New Car() With {.ID = 12, .Trademark = "Dodge", .Model = "Ram 1500", .HP = 215, .Liter = 3.7})
-			list.Add(New Car() With {.ID = 13, .Trademark = "GMC", .Model = "Siera Quadrasteer", .HP = 200, .Liter = 4.3})
-			list.Add(New Car() With {.ID = 14, .Trademark = "Nissan", .Model = "Crew Cab SE", .HP = 143, .Liter = 2.4})
-			list.Add(New Car() With {.ID = 15, .Trademark = "Toyota", .Model = "Tacoma S-Runner", .HP = 190, .Liter = 3.4})
-			Return list
-		End Function
-	End Class
-	Public Class Car
-		Private privateID As Integer
-		Public Property ID() As Integer
-			Get
-				Return privateID
-			End Get
-			Set(ByVal value As Integer)
-				privateID = value
-			End Set
-		End Property
-		Private privateTrademark As String
-		Public Property Trademark() As String
-			Get
-				Return privateTrademark
-			End Get
-			Set(ByVal value As String)
-				privateTrademark = value
-			End Set
-		End Property
-		Private privateModel As String
-		Public Property Model() As String
-			Get
-				Return privateModel
-			End Get
-			Set(ByVal value As String)
-				privateModel = value
-			End Set
-		End Property
-		Private privateHP As Int16
-		Public Property HP() As Int16
-			Get
-				Return privateHP
-			End Get
-			Set(ByVal value As Int16)
-				privateHP = value
-			End Set
-		End Property
-		Private privateLiter As Double
-		Public Property Liter() As Double
-			Get
-				Return privateLiter
-			End Get
-			Set(ByVal value As Double)
-				privateLiter = value
-			End Set
-		End Property
-	End Class
+    Public NotInheritable Class CarList
+
+        Private Sub New()
+        End Sub
+
+        Public Shared Function GetList() As BindingList(Of Car)
+            Dim list = New BindingList(Of Car)()
+            list.Add(New Car() With { _
+                .ID = 1, _
+                .Trademark = "Mercedes-Benz", _
+                .Model = "SL500 Roadster", _
+                .HP = 302, _
+                .Liter = 4.966 _
+            })
+            list.Add(New Car() With { _
+                .ID = 2, _
+                .Trademark = "Mercedes-Benz", _
+                .Model = "CLK55 AMG Cabriolet", _
+                .HP = 342, _
+                .Liter = 5.439 _
+            })
+            list.Add(New Car() With { _
+                .ID = 3, _
+                .Trademark = "Mercedes-Benz", _
+                .Model = "C230 Kompressor Sport Coupe", _
+                .HP = 189, _
+                .Liter = 1.796 _
+            })
+            list.Add(New Car() With { _
+                .ID = 4, _
+                .Trademark = "BMW", _
+                .Model = "530i", _
+                .HP = 225, _
+                .Liter = 3 _
+            })
+            list.Add(New Car() With { _
+                .ID = 5, _
+                .Trademark = "Rolls-Royce", _
+                .Model = "Corniche", _
+                .HP = 325, _
+                .Liter = 6.75 _
+            })
+            list.Add(New Car() With { _
+                .ID = 6, _
+                .Trademark = "Jaguar", _
+                .Model = "S-Type 3.0", _
+                .HP = 235, _
+                .Liter = 3 _
+            })
+            list.Add(New Car() With { _
+                .ID = 7, _
+                .Trademark = "Cadillac", _
+                .Model = "Seville", _
+                .HP = 275, _
+                .Liter = 4.6 _
+            })
+            list.Add(New Car() With { _
+                .ID = 8, _
+                .Trademark = "Cadillac", _
+                .Model = "DeVille", _
+                .HP = 275, _
+                .Liter = 4.6 _
+            })
+            list.Add(New Car() With { _
+                .ID = 9, _
+                .Trademark = "Lexus", _
+                .Model = "LS430", _
+                .HP = 290, _
+                .Liter = 4.3 _
+            })
+            list.Add(New Car() With { _
+                .ID = 10, _
+                .Trademark = "Lexus", _
+                .Model = "GS 430", _
+                .HP = 300, _
+                .Liter = 4.3 _
+            })
+            list.Add(New Car() With { _
+                .ID = 11, _
+                .Trademark = "Ford", _
+                .Model = "Ranger FX-4", _
+                .HP = 135, _
+                .Liter = 2.3 _
+            })
+            list.Add(New Car() With { _
+                .ID = 12, _
+                .Trademark = "Dodge", _
+                .Model = "Ram 1500", _
+                .HP = 215, _
+                .Liter = 3.7 _
+            })
+            list.Add(New Car() With { _
+                .ID = 13, _
+                .Trademark = "GMC", _
+                .Model = "Siera Quadrasteer", _
+                .HP = 200, _
+                .Liter = 4.3 _
+            })
+            list.Add(New Car() With { _
+                .ID = 14, _
+                .Trademark = "Nissan", _
+                .Model = "Crew Cab SE", _
+                .HP = 143, _
+                .Liter = 2.4 _
+            })
+            list.Add(New Car() With { _
+                .ID = 15, _
+                .Trademark = "Toyota", _
+                .Model = "Tacoma S-Runner", _
+                .HP = 190, _
+                .Liter = 3.4 _
+            })
+            Return list
+        End Function
+    End Class
+    Public Class Car
+        Public Property ID() As Integer
+        Public Property Trademark() As String
+        Public Property Model() As String
+        Public Property HP() As Int16
+        Public Property Liter() As Double
+    End Class
 End Namespace
--- VB/Form1.vb
+++ VB/Form1.vb
@@ -1,5 +1,4 @@
-Imports Microsoft.VisualBasic
-Imports System
+﻿Imports System
 Imports System.Collections.Generic
 Imports System.ComponentModel
 Imports System.Data
@@ -11,80 +10,81 @@ Imports DevExpress.XtraGrid
 Imports DevExpress.XtraGrid.Views.Grid
 
 Namespace summary
-	Partial Public Class Form1
-		Inherits Form
-		Public Sub New()
-			InitializeComponent()
-		End Sub
+    Partial Public Class Form1
+        Inherits Form
 
-		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
-			gridControl1.DataSource = CarList.GetList()
-		End Sub
+        Public Sub New()
+            InitializeComponent()
+        End Sub
 
-		Private counter As Integer
-		Private ivalue As Integer
-		Private dvalue As Double
+        Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles Me.Load
+            gridControl1.DataSource = CarList.GetList()
+        End Sub
 
-		Private Sub gridView1_CustomSummaryCalculate(ByVal sender As Object, ByVal e As DevExpress.Data.CustomSummaryEventArgs) Handles gridView1.CustomSummaryCalculate
-			If (Not e.IsGroupSummary) Then
-				Return
-			End If
-			If e.SummaryProcess = CustomSummaryProcess.Start Then
-				ivalue = 0
-				dvalue = 0
-				counter = 0
-			End If
-			If e.SummaryProcess = CustomSummaryProcess.Calculate Then
-				If (CType(e.Item, GridGroupSummaryItem)).FieldName = "HP" Then
-					If e.GroupRowHandle Mod 2 = 0 Then
-						ivalue += CShort(Fix(e.FieldValue))
-					Else
-						ivalue += 1
-					End If
-				End If
-				If (CType(e.Item, GridGroupSummaryItem)).FieldName = "Liter" Then
-					If e.GroupRowHandle Mod 2 = 0 Then
-						dvalue = Math.Max(dvalue, CDbl(e.FieldValue))
-					Else
-						dvalue += CDbl(e.FieldValue)
-						counter += 1
-					End If
-				End If
-			End If
-			If e.SummaryProcess = CustomSummaryProcess.Finalize Then
-				If (CType(e.Item, GridGroupSummaryItem)).FieldName = "HP" Then
-					If e.GroupRowHandle Mod 2 = 0 Then
-						e.TotalValue = ivalue
-					Else
-						e.TotalValue = ivalue
-					End If
-				End If
-				If (CType(e.Item, GridGroupSummaryItem)).FieldName = "Liter" Then
-					If e.GroupRowHandle Mod 2 = 0 Then
-						e.TotalValue = dvalue
-					Else
-						e.TotalValue = dvalue / counter
-					End If
-				End If
-			End If
-		End Sub
+        Private counter As Integer
+        Private ivalue As Integer
+        Private dvalue As Double
 
-		Private Sub gridView1_CustomDrawRowFooterCell(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.FooterCellCustomDrawEventArgs) Handles gridView1.CustomDrawRowFooterCell
-			Dim view As GridView = CType(sender, GridView)
-			If e.Column.FieldName = "HP" Then
-				If e.RowHandle Mod 2 = 0 Then
-					e.Info.DisplayText = String.Format("Total: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(0), GridGroupSummaryItem)))
-				Else
-					e.Info.DisplayText = String.Format("Count: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(0), GridGroupSummaryItem)))
-				End If
-			End If
-			If e.Column.FieldName = "Liter" Then
-				If e.RowHandle Mod 2 = 0 Then
-					e.Info.DisplayText = String.Format("Max: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(1), GridGroupSummaryItem)))
-				Else
-					e.Info.DisplayText = String.Format("Avr: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(1), GridGroupSummaryItem)))
-				End If
-			End If
-		End Sub
-	End Class
+        Private Sub gridView1_CustomSummaryCalculate(ByVal sender As Object, ByVal e As DevExpress.Data.CustomSummaryEventArgs) Handles gridView1.CustomSummaryCalculate
+            If Not e.IsGroupSummary Then
+                Return
+            End If
+            If e.SummaryProcess = CustomSummaryProcess.Start Then
+                ivalue = 0
+                dvalue = 0
+                counter = 0
+            End If
+            If e.SummaryProcess = CustomSummaryProcess.Calculate Then
+                If CType(e.Item, GridGroupSummaryItem).FieldName = "HP" Then
+                    If e.GroupRowHandle Mod 2 = 0 Then
+                        ivalue += CShort(Math.Truncate(e.FieldValue))
+                    Else
+                        ivalue += 1
+                    End If
+                End If
+                If CType(e.Item, GridGroupSummaryItem).FieldName = "Liter" Then
+                    If e.GroupRowHandle Mod 2 = 0 Then
+                        dvalue = Math.Max(dvalue, CDbl(e.FieldValue))
+                    Else
+                        dvalue += CDbl(e.FieldValue)
+                        counter += 1
+                    End If
+                End If
+            End If
+            If e.SummaryProcess = CustomSummaryProcess.Finalize Then
+                If CType(e.Item, GridGroupSummaryItem).FieldName = "HP" Then
+                    If e.GroupRowHandle Mod 2 = 0 Then
+                        e.TotalValue = ivalue
+                    Else
+                        e.TotalValue = ivalue
+                    End If
+                End If
+                If CType(e.Item, GridGroupSummaryItem).FieldName = "Liter" Then
+                    If e.GroupRowHandle Mod 2 = 0 Then
+                        e.TotalValue = dvalue
+                    Else
+                        e.TotalValue = dvalue / counter
+                    End If
+                End If
+            End If
+        End Sub
+
+        Private Sub gridView1_CustomDrawRowFooterCell(ByVal sender As Object, ByVal e As DevExpress.XtraGrid.Views.Grid.FooterCellCustomDrawEventArgs) Handles gridView1.CustomDrawRowFooterCell
+            Dim view As GridView = DirectCast(sender, GridView)
+            If e.Column.FieldName = "HP" Then
+                If e.RowHandle Mod 2 = 0 Then
+                    e.Info.DisplayText = String.Format("Total: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(0), GridGroupSummaryItem)))
+                Else
+                    e.Info.DisplayText = String.Format("Count: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(0), GridGroupSummaryItem)))
+                End If
+            End If
+            If e.Column.FieldName = "Liter" Then
+                If e.RowHandle Mod 2 = 0 Then
+                    e.Info.DisplayText = String.Format("Max: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(1), GridGroupSummaryItem)))
+                Else
+                    e.Info.DisplayText = String.Format("Avr: {0}", view.GetGroupSummaryValue(e.RowHandle, CType(view.GroupSummary(1), GridGroupSummaryItem)))
+                End If
+            End If
+        End Sub
+    End Class
 End Namespace
